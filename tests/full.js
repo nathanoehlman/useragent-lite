0cee08b408e457f3a63165feaae6750a8ae250e2
mflorence99 <mflo999@gmail.com>
2014-09-21 16:42:41 -0400

    v0.0.2

--- tests/full.js
+++ tests/full.js
@@ -1,7 +1,7 @@
 /**
  *
  * @package     useragent-lite
- * @version     0.0.1
+ * @version     0.0.2
  * @copyright   Copyright (c) 2014 - All rights reserved.
  * @license     MIT License
  * @author      Mark Florence <mflo999@gmail.com>
